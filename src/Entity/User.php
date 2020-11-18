6a1a8d8c66d99eaabbdd31d49291c58735eb71ef
accessiproject <kevin.bustamante@maiil.novancia.fr>
2020-11-18 08:04:52 +0100

    remove message entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -55,11 +55,7 @@ class User implements UserInterface
      * @ORM\Column(type="datetime")
      */
     private $createdat;
-
-    /**
-     * @ORM\OneToMany(targetEntity=Message::class, mappedBy="user")
-     */
-    private $messages;
+	
 
     /**
      * @ORM\OneToMany(targetEntity=Article::class, mappedBy="user")
@@ -200,36 +196,6 @@ class User implements UserInterface
     public function setCreatedat(\DateTimeInterface $createdat): self
     {
         $this->createdat = $createdat;
-
-        return $this;
-    }
-
-    /**
-     * @return Collection|Message[]
-     */
-    public function getMessages(): Collection
-    {
-        return $this->messages;
-    }
-
-    public function addMessage(Message $message): self
-    {
-        if (!$this->messages->contains($message)) {
-            $this->messages[] = $message;
-            $message->setUser($this);
-        }
-
-        return $this;
-    }
-
-    public function removeMessage(Message $message): self
-    {
-        if ($this->messages->removeElement($message)) {
-            // set the owning side to null (unless already changed)
-            if ($message->getUser() === $this) {
-                $message->setUser(null);
-            }
-        }
 
         return $this;
     }
